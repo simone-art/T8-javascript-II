cdf3338ad53d8f9b73148a1d9f07222b3dce398c
Evelyn Alves <evelynalves201168@gmail.com>
2019-09-23 17:16:43 -0300

    Corregi exercicio 2

---
+++ Exercício-02/js/script.js
@@ -0,0 +1,30 @@
+
+const imagenGato= document.querySelectorAll (".item__imagem");
+
+    for (let i = 0; i < imagenGato.length; i++) {
+    imagenGato[i].addEventListener ("click", function () {
+        imagenGato[i].classList.add ("invisivel")
+    });   
+};
+
+
+// const item = 0;
+
+
+// let imagenGato= 0
+// for (imagenGato= 0; imagenGato= < imagenGato.length; imagenGato++) {
+//     const item = document.getElementsByClassName ("item__imagen")
+//     item__imagen 
+    
+    
+// }
+
+
+
+// const imagenGato= document.querySelectorAll (".item__imagem");
+
+//     for (let i = 0; i < imagenGato.length; i++) {
+//     imagenGato[i].addEventListener ("click", function () {
+//         imagenGato[i].classList.add ("invisivel")
+// });
+
