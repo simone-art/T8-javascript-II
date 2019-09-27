da1d9fc2d5f30078aa4827c1f332514dc674978f
simone-art <simoneperiodist@gmail.com>
2019-09-27 13:15:19 -0300

    inclui botones nuevos con class adicionadas y removidas, mais eeventListener y usando el for let propiedade in objeto

--- Exercício-05/js/script.js
+++ Exercício-05/js/script.js
@@ -1,9 +1,9 @@
 
 const livros = {
     1: {
-        "titulo": "Java Eloquente",
+        "titulo": "Javascript Eloquente",
         "quemEscreveu": "Marijn Haverbeke",
-        "link": "https://github.com/braziljs/eloquente-java"
+        "link": "https://github.com/braziljs/eloquente-javascript"
     },
     2: {
         "titulo": "Você não sabe JS",
@@ -20,47 +20,82 @@ const livros = {
 const livrosCatalogo = document.getElementById ("lista-do-catalogo")
 
 
-    // (let propiedade in livros) {
+ for  (let propiedade in livros) {
     // livros [propiedade]
-    let livro1 = document.createElement ("p");
-    livro1.textContent = livros[1]["titulo"];
-    let nomeEscritor = document.createElement ("h2");
-    nomeEscritor.textContent= ("Marijn Haverbeke");
-    let linksLivros = document.createElement ("a");
-    linksLivros.textContent = ("https://github.com/braziljs/eloquente-java");
-    linksLivros.setAttribute = ("href", livros [1] ["link"]);
-    livrosCatalogo.appendChild (livro1);
+    let livro = document.createElement ("h2");
+    livro.textContent = livros[propiedade]["titulo"];
+    livrosCatalogo.appendChild (livro);
+    livro.classList.add ("livro__titulo");
+    
+
+    let nomeEscritor = document.createElement ("p");
+    nomeEscritor.textContent= livros [propiedade] ["quemEscreveu"];
     livrosCatalogo.appendChild (nomeEscritor);
+
+
+    let linksLivros = document.createElement ("a");
     livrosCatalogo.appendChild (linksLivros);
-    livro1.classList.add ("livro1");
+    linksLivros.setAttribute ("href", livros[propiedade]["link"]);
+    linksLivros.setAttribute ("target", "_blank");
+    linksLivros.textContent = "Leia aquí";
+    // livrosCatalogo.appendChild (linksLivros);
+    livro.classList.add ("livro__link");
+
+    // linksLivros.textContent = livros [propiedade] ["link"];
+    
+
+    let botones = document.createElement ("button");
+    botones.textContent = "Já li";
+    livrosCatalogo.appendChild (botones);
+    botones.classList.add ("botao-lido");
+
+    botones.addEventListener ("click", function () {
+        livro.classList.add ("livro__titulo--lido")
+    })
+
+    let botonNoLei = document.createElement ("button");
+    botonNoLei.textContent = "Nao li";
+    livrosCatalogo.appendChild (botonNoLei);
+    botonNoLei.classList.add ("botao-desfazer");
+
+    botonNoLei.addEventListener ("click", function () {
+        livro.classList.remove ("livro__titulo--lido")
+    })
+
+}
+
+// NO ES NECESARIO DEFINIR UNA VARIABLE POR CADA LIBRO. 
+//PARA ESO, USAS EL FOR LET PROPIEDADE IN OBJETO PARA 
+//QUE EL MISMO PROCESO SE REPITA PARA CADA ELEMENTO.
+//
 
 
-    let livro2 = document.createElement ("p");
-    livro2.textContent = livros[2]["titulo"];
-    let nomeEscritor2 = document.createElement ("h2");
-    nomeEscritor2.textContent= ("Kyle Simpson");
-    let linksLivros2 = document.createElement ("a");
-    linksLivros2.textContent = ("https://github.com/cezaraugusto/You-Dont-Know-JS");
-    linksLivros2.setAttribute = ("href", livros[2] ["link"]);
-    livrosCatalogo.appendChild (livro2);
-    livrosCatalogo.appendChild (nomeEscritor2);
-    livrosCatalogo.appendChild (linksLivros2);
-    livro2.classList.add ("livro2");
+//     let livro2 = document.createElement ("p");
+//     livro2.textContent = livros[propiedade]["titulo"];
+//     let nomeEscritor2 = document.createElement ("h2");
+//     nomeEscritor2.textContent= livros [propiedade] ["quemEscreveu"];
+//     let linksLivros2 = document.createElement ("a");
+//     linksLivros2.textContent = livros [propiedade] ["link"];
+//     linksLivros2.setAttribute = ("href", livros[propiedade] ["link"]);
+//     livrosCatalogo.appendChild (livro2);
+//     livrosCatalogo.appendChild (nomeEscritor2);
+//     livrosCatalogo.appendChild (linksLivros2);
+//     livro2.classList.add ("livro2");
 
 
-    let livro3 = document.createElement ("p");
-    livro3.textContent = livros[3]["titulo"];
-    let nomeEscritor3 = document.createElement ("h2");
-    nomeEscritor3.textContent= livros[3]["nomeEscritor3"];
-    let linksLivros3 = document.createElement ("a");
-    linksLivros3.textContent = ("http://coletivosycorax.org/wp-content/uploads/2019/09/CALIBA_E_A_BRUXA_WEB-1.pdf");
-    linksLivros3.setAttribute = ("href", livros[3] ["link"]);
-    livrosCatalogo.appendChild (livro3);
-    livrosCatalogo.appendChild (nomeEscritor3);
-    livrosCatalogo.appendChild (linksLivros3);
-    livro3.classList.add ("livro3");
+//     let livro3 = document.createElement ("p");
+//     livro3.textContent = livros[propiedade]["titulo"];
+//     let nomeEscritor3 = document.createElement ("h2");
+//     nomeEscritor3.textContent= livros [propiedade] ["quemEscreveu"];
+//     let linksLivros3 = document.createElement ("a");
+//     linksLivros3.textContent = livros [propiedade] ["link"];
+//     linksLivros3.setAttribute = ("href", livros[propiedade] ["link"]);
+//     livrosCatalogo.appendChild (livro3);
+//     livrosCatalogo.appendChild (nomeEscritor3);
+//     livrosCatalogo.appendChild (linksLivros3);
+//     livro3.classList.add ("livro3");
 
-
+// }
 
 // let livro1 = document.createElement (li);
 // livro1.textContent = (livros.titulo);
@@ -98,8 +133,4 @@ const livrosCatalogo = document.getElementById ("lista-do-catalogo")
 //     hobbie: ["nataçao", "ler", "correr"],
 //     cantoraFavorita: "Sia",
 //     fraseDoDia: "vai dar certo"
-
-// } 
-
-
-
+// }
